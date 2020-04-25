dda7639aa95f93acd296571582b6da2db716d647
brandonolson <brandonolson32@yahoo.com>
2020-04-24 23:57:04 -0400

    blog app

---
+++ src/build/precache-manifest.ca75eb4e3e0ebf6ae2800f7b58c0f5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9efc79d83e88f645c8d6222c356f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7457356f47ba34f825cd",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "5d3cbd7469ef54472410",
+    "url": "/static/js/2.94717509.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.94717509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7457356f47ba34f825cd",
+    "url": "/static/js/main.19bf24e7.chunk.js"
+  },
+  {
+    "revision": "430711db2cc41196af7a",
+    "url": "/static/js/runtime-main.9798f641.js"
+  }
+]);
